98ab2849b69f5dbeb9f609d4230243209524bf5b
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-16 00:59:19 +0530

    Validation of Email

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -8,21 +8,22 @@ public class UserRegistrationProblem
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Enter Last name");
+		System.out.println("Enter Email");
 		
 		Scanner sc = new Scanner(System.in);
-		String lname = sc.next();
+		String email = sc.next();
 		
-		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,30}$");
-		Matcher matcher = pattern.matcher(lname);
+		Pattern pattern = Pattern.compile("^[a-z]{1,}[.]([a-z]*)[@][a-z]{1,}[.]"
+											+ "[a-z]{1,}[.]([a-z]*)");
+		Matcher matcher = pattern.matcher(email);
 		
 		if (matcher.matches())
 		{
-			System.out.println("Valid Last Name");
+			System.out.println("Valid Email");
 		}
 		else
 		{
-			System.out.println("Invalid Last Name");
+			System.out.println("Invalid Email");
 		}
 	}
 }
