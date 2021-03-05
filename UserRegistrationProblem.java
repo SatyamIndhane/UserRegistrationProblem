c6392c4e4b73e2a6989c88badd567a582720a332
SatyamIndhane <satyam.indhane@gmail.com>
2021-03-05 22:08:55 +0530

    Validation according to Rule 1 and 2 and 3

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -13,20 +13,22 @@ public class UserRegistrationProblem
 		Scanner sc = new Scanner(System.in);
 		String pwd = sc.nextLine();
 		
-		Pattern pattern = Pattern.compile("(?=.*[A-Z])[a-zA-Z0-9]{8,}");
+		Pattern pattern = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}");
 		Matcher matcher = pattern.matcher(pwd);//
 		
 		if (matcher.matches())
 		{
 			System.out.println("Valid According to Rule 1 & 2");
 			System.out.println("1 : Must have 8 characters"
-					+ "\n2 : Must have atleast one UpperCase");
+					+ "\n2 : Must have atleast one UpperCase"
+					+ "\n3 : Must have atleast one Numeric");
 		}
 		else
 		{
 			System.out.println("Invalid According to Rule 1 & 2");
 			System.out.println("1 : Must have 8 characters"
-					+ "\n2 : Must have atleast one UpperCase");
+					+ "\n2 : Must have atleast one UpperCase"
+					+ "\n3 : Must have atleast one Numeric");
 		}
 	}
 }
