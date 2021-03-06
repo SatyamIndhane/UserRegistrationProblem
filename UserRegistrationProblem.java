67a3ddd5962f75bfd3d24b9e9cce31e94ce9a195
SatyamIndhane <satyam.indhane@gmail.com>
2021-03-06 23:18:48 +0530

    Validation according to Rule 1 and 2 and 3 and 4

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -11,24 +11,26 @@ public class UserRegistrationProblem
 		System.out.println("Enter Password");
 		
 		Scanner sc = new Scanner(System.in);
-		String pwd = sc.nextLine();
+		String pwd = sc.nextLine(); 
 		
-		Pattern pattern = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}");
-		Matcher matcher = pattern.matcher(pwd);//
+		Pattern pattern = Pattern.compile("(?=[^#?!@$%^&*-]*[#?!@$%^&*-][^#?!@$%^&*-]*$)(?=.*[A-Z])(?=.*[0-9]).{8,}");
+		Matcher matcher = pattern.matcher(pwd);
 		
 		if (matcher.matches())
 		{
-			System.out.println("Valid According to Rule 1 & 2");
+			System.out.println("Valid According to Rule 1 & 2 & 3 & 4");
 			System.out.println("1 : Must have 8 characters"
 					+ "\n2 : Must have atleast one UpperCase"
-					+ "\n3 : Must have atleast one Numeric");
+					+ "\n3 : Must have atleast one Numeric"
+					+ "\n4 : Must have exactly one Special Character");
 		}
 		else
 		{
-			System.out.println("Invalid According to Rule 1 & 2");
+			System.out.println("Invalid According to Rule 1 & 2 & 3 & 4");
 			System.out.println("1 : Must have 8 characters"
 					+ "\n2 : Must have atleast one UpperCase"
-					+ "\n3 : Must have atleast one Numeric");
+					+ "\n3 : Must have atleast one Numeric"
+					+ "\n4 : Must have exactly one Special Character");
 		}
 	}
 }
